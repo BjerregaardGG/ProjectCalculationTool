e24921612b4bd96334c515bca682811d95e1aab5
BjerregaardGG <oliverbjerregaard1@gmail.com>
2024-11-21 09:53:13 +0100

    Initial commit

---
+++ src/main/java/kea/projectcalculationtool/ProjectCalculationToolApplication.java
@@ -0,0 +1,13 @@
+package kea.projectcalculationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalculationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCalculationToolApplication.class, args);
+	}
+
+}
