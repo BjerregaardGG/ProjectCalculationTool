db060d85aa0d71cf686e62a76297663fc6b6978e
Telity <Nigu0001@Stud.kea.dk>
2024-11-26 20:48:52 +0100

    Calculate

--- src/main/java/kea/projectcalculationtool/Project/ProjectController.java
+++ src/main/java/kea/projectcalculationtool/Project/ProjectController.java
@@ -1,4 +1,18 @@
 package kea.projectcalculationtool.Project;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class ProjectController {
+
+    @GetMapping("/project/{projectId}/time")
+    public String showProjectTime(@PathVariable int projectId,Model model) {
+        double totalTime = projectRepository.calculateTime(projectId);
+
+        model.addAttribute("totalTime", totalTime);
+        return "some-project-page";
+    }
 }
