9b2d2174a3846aed7dd2edbc1de6fb9ca815f1cc
Jhockinn <Tobiln@hotmail.dk>
2024-11-26 12:31:14 +0100

    basic constructor and getter and setters

--- src/main/java/kea/projectcalculationtool/Project/ProjectModel.java
+++ src/main/java/kea/projectcalculationtool/Project/ProjectModel.java
@@ -1,4 +1,59 @@
 package kea.projectcalculationtool.Project;
 
+import java.util.Date;
+
 public class ProjectModel {
+    private String projectName;
+    private String projectDescription;
+    private double Budget;
+    private Date startDate;
+    private Date Deadline;
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public double getBudget() {
+        return Budget;
+    }
+
+    public void setBudget(double budget) {
+        Budget = budget;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getDeadline() {
+        return Deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        Deadline = deadline;
+    }
+
+    public ProjectModel(String projectName, String projectDescription, double Budget, Date startDate, Date Deadline) {
+        this.projectName = projectName;
+        this.projectDescription = projectDescription;
+        this.Budget = Budget;
+        this.startDate = startDate;
+        this.Deadline = Deadline;
+    }
 }
