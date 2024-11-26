d208f0ba6e4a4e933d232df9ae5e063e0b9ab10e
Telity <Nigu0001@Stud.kea.dk>
2024-11-26 20:16:50 +0100

    html and css for homepage endpoint to homepage

--- src/main/java/kea/projectcalculationtool/Employee/EmployeeController.java
+++ src/main/java/kea/projectcalculationtool/Employee/EmployeeController.java
@@ -1,4 +1,13 @@
 package kea.projectcalculationtool.Employee;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EmployeeController {
+
+    @GetMapping("/home")
+    public String ShowHomepage(){
+        return "homepage";
+    }
 }
