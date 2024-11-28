d97f7e38b7a683014872b8bed14e37061aa160f1
Telity <Nigu0001@Stud.kea.dk>
2024-11-28 10:33:25 +0100

    Project page, sortha finished homepage set up for view projects and create project also showing the projects that are finished

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO PROJECT (NAME, START_DATE, DEADLINE, BUDGET, DESCRIPTION, STATUS)
+INSERT INTO project (name, start_date, deadline, budget, description, status)
 VALUES ('Project Omega', '2024-01-15', '2024-12-15', 75000.50, 'An innovative project to enhance AI capabilities.', TRUE);
