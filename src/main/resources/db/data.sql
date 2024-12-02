0b91f74f71a3668d45808924fbfb46bb0df57a1f
nikolajpirum <nikolajpirum.10@gmail.com>
2024-12-02 11:16:26 +0100

    added css to button in activeProjects.html. added new project to data.sql. Added button style to homepage.css

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO project (name, start_date, deadline, budget, description, status)
 VALUES ('Project Omega', '2024-01-15', '2024-12-15', 75000.50, 'An innovative project to enhance AI capabilities.', TRUE),
-       ('Project FlyHigh', '1999-01-01', '2099-12-12','5000','Fly to Mars and establish KEA helpdisk', FALSE);
+       ('FlyHigh', '1999-01-01', '2099-12-12','5000','Deport dwarfs to mars', FALSE);
